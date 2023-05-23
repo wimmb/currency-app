ff0da7c44adff9f227b9f8976af59d03724004c5
wimmb <veremeenko.ig@gmail.com>
2023-05-23 11:59:45 +0300

    update

--- core/views.py
+++ core/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from currency.services import PrivatExchangeRatesService
+from currency.services import ProvidersService
 
 
 # Create your views here.
 def index(request):
+
+    providers = [
+        {
+            "name": "Privat Bank",
+            "api_url": "https://api.privatbank.ua/p24api/exchange_rates"
+        }
+    ]
+
+    for p in providers:
+        provider = ProvidersService()
+        provider.create_provider(p)
+
+    service = PrivatExchangeRatesService()
+    objects = service.get_rates()
+    service.persist_currency_rates(objects)
     return render(request, "core/index.html")
--- currency/models.py
+++ currency/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 
-# Create your models here.
-# class Rate(models.Model):
-#     name = models.CharField(max_length=100)
-#
-#     def __str__(self):
-#         return self.name
+class ExchangeRateProvider(models.Model):
+    name = models.CharField(max_length=50)
+    api_url = models.URLField()
+
+
+class ExchangeRate(models.Model):
+    base_currency = models.CharField(max_length=20)
+    currency = models.CharField(max_length=20)
+
+    buy_rate = models.DecimalField(max_digits=10, decimal_places=4)
+    sale_rate = models.DecimalField(max_digits=10, decimal_places=4)
+
+    date = models.CharField(max_length=20)
+
+    provider = models.ForeignKey(ExchangeRateProvider, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.base_currency}/{self.currency}'
