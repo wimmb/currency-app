d73772af8bf3e71083be8e43e11d7d951e52a8af
wimmb <veremeenko.ig@gmail.com>
2023-05-23 16:06:36 +0300

    update

--- core/views.py
+++ core/views.py
@@ -14,11 +14,11 @@ def index(request):
     provider_service = ProvidersService(name=bank.get('name'), api_url=bank.get('api_url'))
     provider = provider_service.get_or_create()
 
-    # Создание объекта ExchangeRatesService с передачей provider в конструктор
+    # Создание объекта PrivatExchangeRatesService с передачей provider в конструктор
     exchange_service = PrivatExchangeRatesService(provider=provider)
 
     objects = exchange_service.get_rates()
-    print(objects)
-    # exchange_service.persist_currency_rates(objects)
+    # print(objects)
+    exchange_service.persist_currency_rates(objects)
 
     return render(request, "core/index.html")
